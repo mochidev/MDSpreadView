d8aeeda7ee09856b1046811edbdff9a1023a2713
Dimitri Bouniol <dimitribouniol@mochidev.com>
2011-10-15 20:18:12 -0700

    first commit

---
+++ MDSpreadView.h
@@ -0,0 +1,13 @@
+//
+//  MDSpreadView.h
+//  MDSpreadViewDemo
+//
+//  Created by Dimitri Bouniol on 10/15/11.
+//  Copyright (c) 2011 Mochi Development, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MDSpreadView : UIView
+
+@end
---
+++ MDSpreadView.m
@@ -0,0 +1,31 @@
+//
+//  MDSpreadView.m
+//  MDSpreadViewDemo
+//
+//  Created by Dimitri Bouniol on 10/15/11.
+//  Copyright (c) 2011 Mochi Development, Inc. All rights reserved.
+//
+
+#import "MDSpreadView.h"
+
+@implementation MDSpreadView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
