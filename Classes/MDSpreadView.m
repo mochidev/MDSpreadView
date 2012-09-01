df4a1ee27020111764930b421e8d7fd4030ae225
Dimitri Bouniol <dimitribouniol@mochidev.com>
2012-09-01 13:36:35 -0700

    fixed crash with multitouch

--- Classes/MDSpreadView.m
+++ Classes/MDSpreadView.m
@@ -2955,7 +2955,8 @@
     }
     
     
-    NSArray *allSelections = [_selectedCells arrayByAddingObject:_currentSelection];
+    NSMutableArray *allSelections = [[_selectedCells mutableCopy] autorelease];
+    if (_currentSelection) [allSelections addObject:_currentSelection];
     NSMutableSet *allVisibleCells = [NSMutableSet setWithSet:[self _allVisibleCells]];
     [allVisibleCells addObjectsFromArray:_headerColumnCells];
     [allVisibleCells addObjectsFromArray:_headerRowCells];
